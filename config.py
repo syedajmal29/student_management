dfcdaf9127bf1a679e755f6e39e2a2f8efc540a0
syedaju398@gmail.com <syedaju398@gmail.com>
2024-11-27 00:34:16 +0400

    update

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(32))
+   
+    SECRET_KEY = os.urandom(32)
     
-    # Use DATABASE_URL environment variable in Render
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Mohammed1@localhost:5432/student_management'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
+    
     UPLOAD_FOLDER = 'static/uploads'
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
-
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  
+    
+    
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
